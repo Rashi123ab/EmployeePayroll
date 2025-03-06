ba003e8ded75a6aeadc5e8d6164a35a360cb1d80
Rashi123ab <rashia729@gmail.com>
2025-03-06 10:53:32 +0530

    Get Post request

--- src/main/java/com/rashi/EmployeePayroll/employee/service/EmployeeService.java
+++ src/main/java/com/rashi/EmployeePayroll/employee/service/EmployeeService.java
@@ -1,23 +1,37 @@
 package com.rashi.EmployeePayroll.employee.service;
-
 import com.rashi.EmployeePayroll.employee.model.Employee;
+import com.rashi.EmployeePayroll.employee.DTO.EmployeeDTO;
 import com.rashi.EmployeePayroll.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository repository;
 
-    public List<Employee> getAllEmployees() {
-        return repository.findAll(); }
-    public Employee getEmployeeById(Long id) {
-        return repository.findById(id).orElse(null); }
-    public Employee saveEmployee(Employee employee) {
-        return repository.save(employee); }
+    public List<EmployeeDTO> getAllEmployees() {
+        return repository.findAll()
+                .stream()
+                .map(emp -> new EmployeeDTO(emp.getName(), emp.getSalary()))
+                .collect(Collectors.toList());
+    }
+
+    public EmployeeDTO getEmployeeById(Long id) {
+        Employee emp = repository.findById(id).orElse(null);
+        return emp != null ? new EmployeeDTO(emp.getName(), emp.getSalary()) : null;
+    }
+
+    public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee(employeeDTO.getName(), employeeDTO.getSalary());
+        Employee savedEmployee = repository.save(employee);
+        return new EmployeeDTO(savedEmployee.getName(), savedEmployee.getSalary());
+    }
+
     public void deleteEmployee(Long id) {
-        repository.deleteById(id); }
+        repository.deleteById(id);
+    }
 }

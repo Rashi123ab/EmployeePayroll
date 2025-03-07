87d5dc2eab36177fa55beecd9aacb27732a52ccf
Rashi123ab <rashia729@gmail.com>
2025-03-07 12:05:38 +0530

    Add Lombok annotation

--- src/main/java/com/rashi/EmployeePayroll/employee/model/Employee.java
+++ src/main/java/com/rashi/EmployeePayroll/employee/model/Employee.java
@@ -1,8 +1,14 @@
 package com.rashi.EmployeePayroll.employee.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,13 +24,13 @@ public class Employee {
     }
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
+//    public Long getId() { return id; }
+//    public void setId(Long id) { this.id = id; }
+//
+//    public String getName() { return name; }
+//    public void setName(String name) { this.name = name; }
+//
+//    public double getSalary() { return salary; }
+//    public void setSalary(double salary) { this.salary = salary; }
 }
 
